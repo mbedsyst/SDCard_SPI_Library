ae68ba638cbf5aed75dbce30e97d62d4314d66e2
Zalman Ul Fariz P A <103169802+mbedsyst@users.noreply.github.com>
2024-11-26 23:30:56 +0530

    Designed SD Card Source file functions

---
+++ Src/SDCard.c
@@ -0,0 +1,16 @@
+#include "SDCard.h"
+
+void SD_Reset(void)
+{
+
+}
+
+void SD_WriteBlock(uint32_t address, uint8_t *data, uint16_t size)
+{
+
+}
+
+void SD_ReadBlock(uint32_t address, uint8_t *data, uint16_t size)
+{
+
+}
